55401f985673d32dfe0d834b38a0870af535371a
summy00 <summy00@qq.com>
2014-11-04 20:54:54 +0800

    Modify project name.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("OCR")]
+[assembly: AssemblyTitle("ScreenCaptureReconizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OCR")]
+[assembly: AssemblyProduct("ScreenCaptureReconizer")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
